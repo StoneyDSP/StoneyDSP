4de0b2e926b81bf3c1020c2902ba9fd4c110e990
Nathan Hood <nathanjhood@googlemail.com>
2022-06-24 16:47:20 +0200

    Added Transforms in custom module & namespace :)

---
+++ modules/stoneydsp_filters/biquads/stoneydsp_Transforms.cpp
@@ -0,0 +1,156 @@
+/*/*
+  ==============================================================================
+
+    Transforms.cpp
+    Created: 16 May 2022 2:54:47am
+    Author:  StoneyDSP
+
+  ==============================================================================
+*/
+
+#include "stoneydsp_Transforms.h"
+
+namespace stoneydsp
+{
+namespace filters
+{
+
+//==============================================================================
+template <typename SampleType>
+Transforms<SampleType>::Transforms()
+{
+    reset(static_cast<SampleType>(0.0));
+}
+
+//==============================================================================
+template <typename SampleType>
+void Transforms<SampleType>::setTransformType(TransformType newTransformType)
+{
+    if (transformType != newTransformType)
+    {
+        transformType = newTransformType;
+        reset(static_cast<SampleType>(0.0));
+    }
+}
+
+//==============================================================================
+template <typename SampleType>
+void Transforms<SampleType>::prepare(int numChannels, double sampleRate)
+{
+    Wn_1.resize(numChannels);
+    Wn_2.resize(numChannels);
+    Xn_1.resize(numChannels);
+    Xn_2.resize(numChannels);
+    Yn_1.resize(numChannels);
+    Yn_2.resize(numChannels);
+
+    reset(static_cast<SampleType>(0.0));
+}
+
+template <typename SampleType>
+void Transforms<SampleType>::reset(SampleType initialValue)
+{
+    for (auto v : { &Wn_1, &Wn_2, &Xn_1, &Xn_2, &Yn_1, &Yn_2 })
+        std::fill(v->begin(), v->end(), initialValue);
+}
+
+template <typename SampleType>
+SampleType Transforms<SampleType>::processSample(int channel, SampleType inputValue)
+{
+    switch (transformType)
+    {
+    case TransformationType::directFormI:
+        inputValue = directFormI(channel, inputValue);
+        break;
+    case TransformationType::directFormII:
+        inputValue = directFormII(channel, inputValue);
+        break;
+    case TransformationType::directFormItransposed:
+        inputValue = directFormITransposed(channel, inputValue);
+        break;
+    case TransformationType::directFormIItransposed:
+        inputValue = directFormIITransposed(channel, inputValue);
+        break;
+    default:
+        inputValue = directFormIITransposed(channel, inputValue);
+    }
+
+    return inputValue;
+}
+
+template <typename SampleType>
+SampleType Transforms<SampleType>::directFormI(int channel, SampleType inputValue)
+{
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+    auto& Yn1 = Yn_1[(size_t)channel];
+    auto& Yn2 = Yn_2[(size_t)channel];
+
+    auto& Xn = inputValue;
+
+    SampleType Yn = ((Xn * b0) + (Xn1 * b1) + (Xn2 * b2) + (Yn1 * a1) + (Yn2 * a2));
+
+    Xn2 = Xn1, Yn2 = Yn1;
+    Xn1 = Xn, Yn1 = Yn;
+
+    return Yn;
+}
+
+template <typename SampleType>
+SampleType Transforms<SampleType>::directFormII(int channel, SampleType inputValue)
+{
+    auto& Wn1 = Wn_1[(size_t)channel];
+    auto& Wn2 = Wn_2[(size_t)channel];
+
+    SampleType Xn = inputValue;
+
+    SampleType Wn = (Xn + ((Wn1 * a1) + (Wn2 * a2)));
+    SampleType Yn = ((Wn * b0) + (Wn1 * b1) + (Wn2 * b2));
+
+    Wn2 = Wn1;
+    Wn1 = Wn;
+
+    return Yn;
+}
+
+template <typename SampleType>
+SampleType Transforms<SampleType>::directFormITransposed(int channel, SampleType inputValue)
+{
+    auto& Wn1 = Wn_1[(size_t)channel];
+    auto& Wn2 = Wn_2[(size_t)channel];
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+
+    SampleType Xn = inputValue;
+
+    SampleType Wn = (Xn + Wn2);
+    SampleType Yn = ((Wn * b0) + Xn2);
+
+    Xn2 = ((Wn * b1) + Xn1), Wn2 = ((Wn * a1) + Wn1);
+    Xn1 = (Wn * b2), Wn1 = (Wn * a2);
+
+    return Yn;
+}
+
+template <typename SampleType>
+SampleType Transforms<SampleType>::directFormIITransposed(int channel, SampleType inputValue)
+{
+    auto& Xn1 = Xn_1[(size_t)channel];
+    auto& Xn2 = Xn_2[(size_t)channel];
+
+    SampleType Xn = inputValue;
+
+    SampleType Yn = ((Xn * b0) + (Xn2));
+
+    Xn2 = ((Xn * b1) + (Xn1)+(Yn * a1));
+    Xn1 = ((Xn * b2) + (Yn * a2));
+
+    return Yn;
+}
+
+//==============================================================================
+template class Transforms<float>;
+template class Transforms<double>;
+
+}
+}
---
+++ modules/stoneydsp_filters/biquads/stoneydsp_Transforms.h
@@ -0,0 +1,85 @@
+/*
+  ==============================================================================
+
+    Transforms.h
+    Created: 16 May 2022 2:54:47am
+    Author:  StoneyDSP
+
+  ==============================================================================
+*/
+
+namespace stoneydsp
+{
+namespace filters
+{
+
+enum class TransformType
+{
+    directFormI = 0,
+    directFormII = 1,
+    directFormItransposed = 2,
+    directFormIItransposed = 3
+};
+
+/**
+    A handy set of transforms.
+*/
+
+template <typename SampleType>
+class Transforms
+{
+public:
+    //==============================================================================
+    /** Constructor. */
+    Transforms() = default;
+
+    //==============================================================================
+    /** Sets the BiLinear Transform for the filter to use. See enum for availa ble types. */
+    void setTransformType(TransformType newTransformType);
+
+    //==============================================================================
+    /** Initialises the processor. */
+    void prepare(int numChannels, double sampleRate);
+
+    /** Resets the internal state variables of the processor. */
+    void reset(SampleType initialValue);
+
+    //==============================================================================
+    /** Processes one sample at a time on a given channel. */
+    SampleType processSample(int channel, SampleType inputValue);
+
+    //==============================================================================
+    SampleType directFormI(int channel, SampleType inputValue);
+    SampleType directFormII(int channel, SampleType inputValue);
+    SampleType directFormITransposed(int channel, SampleType inputValue);
+    SampleType directFormIITransposed(int channel, SampleType inputValue);
+
+    //==============================================================================
+    SampleType& getb0() { return b0; }
+    SampleType& getb1() { return b1; }
+    SampleType& getb2() { return b2; }
+    SampleType& geta0() { return a0; }
+    SampleType& geta1() { return a1; }
+    SampleType& geta2() { return a2; }
+
+private:
+    //==============================================================================
+    /** Unit-delay objects. */
+    std::vector<SampleType> Wn_1, Wn_2, Xn_1, Xn_2, Yn_1, Yn_2;
+
+    //==============================================================================
+    /** Initialise the coefficient gains. */
+    SampleType b0 = 1.0;
+    SampleType b1 = 0.0;
+    SampleType b2 = 0.0;
+    SampleType a0 = 1.0;
+    SampleType a1 = 0.0;
+    SampleType a2 = 0.0;
+
+    //==============================================================================
+    /** Initialise the parameters. */
+    TransformType transformType = TransformType::directFormIItransposed;
+};
+
+} //namespace stoneydsp
+} //
