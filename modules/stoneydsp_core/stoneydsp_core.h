fdcad5b490268abd39a8fcafe0b1d0361647dbb7
Nathan Hood <nathanjhood@googlemail.com>
2022-07-02 05:12:31 +0200

    Began stoneydsp_core module

---
+++ modules/stoneydsp_core/stoneydsp_core.h
@@ -0,0 +1,17 @@
+
+/** BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:             stoneydsp_core
+    vendor:         stoneydsp
+    version:        0.1.1
+    name:           StoneyDSP Core classes
+    description:    Lightweight core parts as classes
+    website:        github.com/StoneyDSP
+    license:        Proprietary  
+
+    END_JUCE_MODULE_DECLARATION
+*/
+
+#pragma once
+
+#define STONEYDSP_CORE_H_INCLUDED
