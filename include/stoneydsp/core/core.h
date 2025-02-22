2187769995368a6d3a7c48954848954e9433024e
StoneyDSP <nathanjhood@googlemail.com>
2025-02-22 20:05:46 +0100

    edit 'include/stoneydsp/core/core.h' - add ptrdiff, nullptr, max_align
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/stoneydsp/core/core.h
+++ include/stoneydsp/core/core.h
@@ -162,9 +162,13 @@ extern char const *stoneydsp_version;
 
 // for `stoneydsp_size_t`
   #include "stoneydsp/core/types/size.h"
+// for `stoneydsp_ptrdiff_t`
+  #include "stoneydsp/core/types/ptrdiff.h"
+// for `stoneydsp_nullptr_t`
+  #include "stoneydsp/core/types/nullptr.h"
+// for `stoneydsp_max_align_t`
+  #include "stoneydsp/core/types/max_align.h"
 
-// for `stoneydsp_size_t`
-  #include "stoneydsp/core/types/def.h"
 // for `stoneydsp_float_t`
   #include "stoneydsp/core/types/math.h"
 
