6943e94422cd1fc914416f1196cccf4765028a44
StoneyDSP <nathanjhood@googlemail.com>
2025-02-24 18:54:49 +0100

    add 'src/stoneydsp/stoneydsp.m'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/stoneydsp/stoneydsp.m
@@ -0,0 +1,12 @@
+/**
+ * @file stoneydsp.m
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+//==============================================================================
+
+#include "stoneydsp.c"
+
+//=========================================================================//EOF
