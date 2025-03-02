89fcaeb0be0923ba4654102812598afdb6200ed2
StoneyDSP <nathanjhood@googlemail.com>
2025-03-02 23:58:13 +0100

    edit 'test/stoneydsp/core/types/ulong.test.cpp' - use custom test macros

--- test/stoneydsp/core/types/ulong.test.cpp
+++ test/stoneydsp/core/types/ulong.test.cpp
@@ -15,74 +15,99 @@
 
 //==============================================================================
 
-  #include "stoneydsp/core/system/compiler.h" // for `STONEYDSP_PUBLIC_FUNCTION`
-  #include <algorithm>                        // for `std::sort`
-  #include <catch2/benchmark/catch_benchmark.hpp> //
-  #include <catch2/catch_test_macros.hpp>         //
-  #include <limits>                               // for `std::numeric_limits`
-  #include <numeric>                              // for `std::accumulate`
-  #include <sstream>                              // for serialization tests
-  #include <type_traits> // for `is_signed` and `is_unsigned`
-  #include <vector>      // for compatibility tests
+  #include "utils.test.hpp"
 
 //======================================================================//sizeof
 
-TEST_CASE ("sizeof stoneydsp::ulong_t is 8 bytes",
-           "[core][types][ulong_t][sizeof]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][sizeof]",
+                     "[core][types][ulong_t][sizeof]")
 {
-  REQUIRE (sizeof (::stoneydsp::ulong_t) == 8UL);
+  STONEYDSP_REQUIRE (sizeof (::stoneydsp::ulong_t) == 8UL);
 }
 
 //=====================================================================//alignof
 
-TEST_CASE ("alignof stoneydsp::ulong_t is 8 bytes",
-           "[core][types][ulong_t][alignof]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][alignof]",
+                     "[core][types][ulong_t][alignof]")
 {
-  REQUIRE (alignof (::stoneydsp::ulong_t) == 8UL);
+  STONEYDSP_REQUIRE (alignof (::stoneydsp::ulong_t) == 8UL);
 }
 
 //=================================================================//type_traits
 
-TEST_CASE ("Check if types are signed or unsigned",
-           "[core][types][ulong_t][type_traits][is_unsigned]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_integral]",
+                     "[core][types][ulong_t][type_traits][is_integral]")
 {
-  REQUIRE (::std::is_unsigned< ::stoneydsp::ulong_t>::value);
+  STONEYDSP_REQUIRE (::std::is_integral< ::stoneydsp::ulong_t>::value);
 }
 
-TEST_CASE ("Is stoneydsp::ulong_t trivially copyable",
-           "[core][types][ulong_t][type_traits][is_trivially_copyable]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_unsigned]",
+                     "[core][types][ulong_t][type_traits][is_unsigned]")
 {
-  REQUIRE (::std::is_trivially_copyable< ::stoneydsp::ulong_t>::value);
+  STONEYDSP_REQUIRE (::std::is_unsigned< ::stoneydsp::ulong_t>::value);
 }
 
-TEST_CASE ("Is stoneydsp::ulong_t standard-layout conforming",
-           "[core][types][ulong_t][type_traits][is_standard_layout]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_arithmetic]",
+                     "[core][types][ulong_t][type_traits][is_arithmetic]")
 {
-  REQUIRE (::std::is_standard_layout< ::stoneydsp::ulong_t>::value);
+  STONEYDSP_REQUIRE (::std::is_arithmetic< ::stoneydsp::ulong_t>::value);
 }
 
-//==============================================================//special_values
-
-TEST_CASE ("Numeric limits of stoneydsp::ulong_t",
-           "[core][types][ulong_t][numeric_limits][special_values]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_pointer]",
+                     "[core][types][ulong_t][type_traits][is_pointer]")
 {
-  // { 0U }
-  REQUIRE (::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::min ()
-           == ::std::numeric_limits< ::stoneydsp::ulong_t>::min ());
+  STONEYDSP_REQUIRE (!::std::is_pointer< ::stoneydsp::ulong_t>::value);
+}
 
-  // { 18446744073709551615UL }
-  REQUIRE (::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::max ()
-           == ::std::numeric_limits< ::stoneydsp::ulong_t>::max ());
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_const]",
+                     "[core][types][ulong_t][type_traits][is_const]")
+{
+  STONEYDSP_REQUIRE (!::std::is_const< ::stoneydsp::ulong_t>::value);
+}
 
-  // { 0U }
-  REQUIRE (::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::lowest ()
-           == ::std::numeric_limits< ::stoneydsp::ulong_t>::lowest ());
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_empty]",
+                     "[core][types][ulong_t][type_traits][is_empty]")
+{
+  STONEYDSP_REQUIRE (!::std::is_empty< ::stoneydsp::ulong_t>::value);
+}
+
+STONEYDSP_TEST_CASE (
+    "[core][types][ulong_t][type_traits][is_trivially_copyable]",
+    "[core][types][ulong_t][type_traits][is_trivially_copyable]")
+{
+  STONEYDSP_REQUIRE (
+      ::std::is_trivially_copyable< ::stoneydsp::ulong_t>::value);
+}
+
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][type_traits][is_standard_layout]",
+                     "[core][types][ulong_t][type_traits][is_standard_layout]")
+{
+  STONEYDSP_REQUIRE (::std::is_standard_layout< ::stoneydsp::ulong_t>::value);
 }
 
 //==============================================================//numeric_limits
 
-TEST_CASE ("Special values of stoneydsp::ulong_t",
-           "[core][types][ulong_t][numeric_limits]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][numeric_limits]",
+                     "[core][types][ulong_t][numeric_limits]")
+{
+  // { 0U }
+  STONEYDSP_REQUIRE (::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::min ()
+                     == ::std::numeric_limits< ::stoneydsp::ulong_t>::min ());
+
+  // { 18446744073709551615UL }
+  STONEYDSP_REQUIRE (::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::max ()
+                     == ::std::numeric_limits< ::stoneydsp::ulong_t>::max ());
+
+  // { 0U }
+  STONEYDSP_REQUIRE (
+      ::stoneydsp::numeric_limits< ::stoneydsp::ulong_t>::lowest ()
+      == ::std::numeric_limits< ::stoneydsp::ulong_t>::lowest ());
+}
+
+//==============================================================//special_values
+
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][special_values]",
+                     "[core][types][ulong_t][special_values]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
@@ -90,15 +115,15 @@ TEST_CASE ("Special values of stoneydsp::ulong_t",
       = ::std::numeric_limits< ::stoneydsp::ulong_t>::min ();
   ::stoneydsp::ulong_t max_val
       = ::std::numeric_limits< ::stoneydsp::ulong_t>::max ();
-  REQUIRE (min_val == 0_ulong_t); // Minimum value for ulong_t
-  REQUIRE (max_val
-           == 18446744073709551615_ulong_t); // Maximum value for ulong_t
+  STONEYDSP_REQUIRE (min_val == 0_ulong_t); // Minimum value for ulong_t
+  STONEYDSP_REQUIRE (
+      max_val == 18446744073709551615_ulong_t); // Maximum value for ulong_t
 }
 
 //==================================================================//endianness
 
-TEST_CASE ("Endianness handling for stoneydsp::ulong_t",
-           "[core][types][ulong_t][endianness]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][endianness]",
+                     "[core][types][ulong_t][endianness]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
@@ -107,49 +132,50 @@ TEST_CASE ("Endianness handling for stoneydsp::ulong_t",
 
   if (bytePtr[0] == 0xF0)
     {
-      REQUIRE (true); // Little-endian
+      STONEYDSP_REQUIRE (true); // Little-endian
     }
   else if (bytePtr[0] == 0x12)
     {
-      REQUIRE (true); // Big-endian
+      STONEYDSP_REQUIRE (true); // Big-endian
     }
   else
     {
-      REQUIRE (false); // Unknown endianness
+      STONEYDSP_REQUIRE (false); // Unknown endianness
     }
 }
 
 //====================================================================//rounding
 
-TEST_CASE ("Rounding behavior of stoneydsp::ulong_t",
-           "[core][types][ulong_t][rounding]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][rounding]",
+                     "[core][types][ulong_t][rounding]")
 {
   ::stoneydsp::float_t a = 1.5f;
   ::stoneydsp::ulong_t b
       = static_cast< ::stoneydsp::ulong_t> (::std::round (a));
-  REQUIRE (b == 2); // 1.5 rounded to nearest integer and cast to ulong_t
+  STONEYDSP_REQUIRE (
+      b == 2); // 1.5 rounded to nearest integer and cast to ulong_t
 }
 
 //==================================================================//arithmetic
 
-TEST_CASE ("Arithmetic operations with stoneydsp::ulong_t",
-           "[core][types][ulong_t][arithmetic]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][arithmetic]",
+                     "[core][types][ulong_t][arithmetic]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1500000000000000000_ulong_t;
   ::stoneydsp::ulong_t b = 2_ulong_t;
-  REQUIRE (a + b == 1500000000000000002_ulong_t); // Addition
-  REQUIRE (a - b == 1499999999999999998_ulong_t); // Subtraction
-  REQUIRE (a * b == 3000000000000000000_ulong_t); // Multiplication
-  REQUIRE (b / a == 0_ulong_t);                   // Division
-  REQUIRE (b % a == 2_ulong_t);                   // Modulo
+  STONEYDSP_REQUIRE (a + b == 1500000000000000002_ulong_t); // Addition
+  STONEYDSP_REQUIRE (a - b == 1499999999999999998_ulong_t); // Subtraction
+  STONEYDSP_REQUIRE (a * b == 3000000000000000000_ulong_t); // Multiplication
+  STONEYDSP_REQUIRE (b / a == 0_ulong_t);                   // Division
+  STONEYDSP_REQUIRE (b % a == 2_ulong_t);                   // Modulo
 }
 
-//===============================================================//bitwise
+//=====================================================================//bitwise
 
-TEST_CASE ("Bitwise operations with stoneydsp::ulong_t",
-           "[core][types][ulong_t][bitwise]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][bitwise][logic]",
+                     "[core][types][ulong_t][bitwise][logic]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
@@ -157,49 +183,49 @@ TEST_CASE ("Bitwise operations with stoneydsp::ulong_t",
   ::stoneydsp::ulong_t a = 5_ulong_t; // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0101
   ::stoneydsp::ulong_t b = 3_ulong_t; // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0011
 
-  REQUIRE ((a & b) == 1); // AND: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0001
-  REQUIRE ((a | b) == 7); // OR:  0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0111
-  REQUIRE ((a ^ b) == 6); // XOR: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0110
-  REQUIRE (
+  STONEYDSP_REQUIRE ((a & b) == 1); // AND: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0001
+  STONEYDSP_REQUIRE ((a | b) == 7); // OR:  0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0111
+  STONEYDSP_REQUIRE ((a ^ b) == 6); // XOR: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0110
+  STONEYDSP_REQUIRE (
       (~a & 0xFFFFFFFFFFFFFFFFUL)
       == 18446744073709551610UL); // NOT: 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1111 1010 (considering ulong_t wrap-around)
 	// clang-format off
 }
 
-TEST_CASE ("Shift operations with stoneydsp::ulong_t", "[core][types][ulong_t][bitwise]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][bitwise][arithmetic]", "[core][types][ulong_t][bitwise][arithmetic]")
 {
 	using namespace ::stoneydsp::core::types::literals;
 
 	// clang-format off
   ::stoneydsp::ulong_t a = 5_ulong_t; // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0101
 
-	REQUIRE ((a << 1) == 10);  // Left shift: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 1010
-  REQUIRE ((a >> 1) == 2);   // Right shift: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0010
+	STONEYDSP_REQUIRE ((a << 1) == 10);  // Left shift: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 1010
+  STONEYDSP_REQUIRE ((a >> 1) == 2);   // Right shift: 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0010
   // clang-format on
 }
 
 //===============================================================//comparison
 
-TEST_CASE ("Comparison operations with stoneydsp::ulong_t",
-           "[core][types][ulong_t][comparison]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][comparison]",
+                     "[core][types][ulong_t][comparison]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 5_ulong_t;
   ::stoneydsp::ulong_t b = 3_ulong_t;
 
-  REQUIRE ((a == b) == false);
-  REQUIRE ((a != b) == true);
-  REQUIRE ((a > b) == true);
-  REQUIRE ((a < b) == false);
-  REQUIRE ((a >= b) == true);
-  REQUIRE ((a <= b) == false);
+  STONEYDSP_REQUIRE ((a == b) == false);
+  STONEYDSP_REQUIRE ((a != b) == true);
+  STONEYDSP_REQUIRE ((a > b) == true);
+  STONEYDSP_REQUIRE ((a < b) == false);
+  STONEYDSP_REQUIRE ((a >= b) == true);
+  STONEYDSP_REQUIRE ((a <= b) == false);
 }
 
 //===============================================================//serialization
 
-TEST_CASE ("Check serialization and deserialization for stoneydsp::ulong_t",
-           "[core][types][ulong_t][serialization]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][serialization]",
+                     "[core][types][ulong_t][serialization]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
@@ -212,42 +238,42 @@ TEST_CASE ("Check serialization and deserialization for stoneydsp::ulong_t",
   ss.read (reinterpret_cast<char *> (&deserializedValue),
            sizeof (deserializedValue));
 
-  REQUIRE (originalValue == deserializedValue);
+  STONEYDSP_REQUIRE (originalValue == deserializedValue);
 }
 
 //=======================================================//boundary_and_overflow
 
-TEST_CASE ("Boundary and overflow behaviour of stoneydsp::ulong_t",
-           "[core][types][ulong_t][boundary][overflow]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][boundary][overflow]",
+                     "[core][types][ulong_t][boundary][overflow]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 18446744073709551615_ulong_t;
   ::stoneydsp::ulong_t b = 1_ulong_t;
 
-  REQUIRE (static_cast< ::stoneydsp::ulong_t> (a + b)
-           == 0_ulong_t); // Check overflow wrap-around behaviour
+  STONEYDSP_REQUIRE (static_cast< ::stoneydsp::ulong_t> (a + b)
+                     == 0_ulong_t); // Check overflow wrap-around behaviour
 }
 
 //======================================================//boundary_and_underflow
 
-TEST_CASE ("Boundary and underflow behavior of stoneydsp::ulong_t",
-           "[core][types][ulong_t][boundary][underflow]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][boundary][underflow]",
+                     "[core][types][ulong_t][boundary][underflow]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 0_ulong_t;
   ::stoneydsp::ulong_t b = 1_ulong_t;
 
-  REQUIRE (
+  STONEYDSP_REQUIRE (
       static_cast< ::stoneydsp::ulong_t> (a - b)
       == 18446744073709551615_ulong_t); // Check underflow wrap-around behavior
 }
 
 //===============================================================//compatibility
 
-TEST_CASE ("Compatibility of stoneydsp::ulong_t with standard library",
-           "[core][types][ulong_t][compatibility]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][compatibility]",
+                     "[core][types][ulong_t][compatibility]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
@@ -258,90 +284,91 @@ TEST_CASE ("Compatibility of stoneydsp::ulong_t with standard library",
   ::std::sort (vec.begin (), vec.end ());
 
   // Verify the vector is sorted
-  REQUIRE (vec
-           == ::std::vector< ::stoneydsp::ulong_t>{
-               1_ulong_t, 2_ulong_t, 3_ulong_t, 4_ulong_t, 5_ulong_t });
+  STONEYDSP_REQUIRE (
+      vec
+      == ::std::vector< ::stoneydsp::ulong_t>{ 1_ulong_t, 2_ulong_t, 3_ulong_t,
+                                               4_ulong_t, 5_ulong_t });
 
   // Use std::accumulate to sum the elements
   ::stoneydsp::ulong_t sum = ::std::accumulate (
       vec.begin (), vec.end (), ::stoneydsp::ulong_t (0_ulong_t));
 
   // Verify the sum is correct
-  REQUIRE (sum == 15_ulong_t);
+  STONEYDSP_REQUIRE (sum == 15_ulong_t);
 }
 
 //===================================================================//benchmark
 
 // Benchmark for addition
-TEST_CASE ("Benchmark for stoneydsp::ulong_t addition",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][addition]",
+                     "[core][types][ulong_t][benchmark][addition]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1200_ulong_t;
   ::stoneydsp::ulong_t b = 3400_ulong_t;
 
-  BENCHMARK ("Addition") { return a + b; };
+  STONEYDSP_BENCHMARK ("Addition") { return a + b; };
 }
 
 // Benchmark for subtraction
-TEST_CASE ("Benchmark for stoneydsp::ulong_t subtraction",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][subtraction]",
+                     "[core][types][ulong_t][benchmark][subtraction]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 3400_ulong_t;
   ::stoneydsp::ulong_t b = 1200_ulong_t;
 
-  BENCHMARK ("Subtraction") { return a - b; };
+  STONEYDSP_BENCHMARK ("Subtraction") { return a - b; };
 }
 
 // Benchmark for multiplication
-TEST_CASE ("Benchmark for stoneydsp::ulong_t multiplication",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][multiplication]",
+                     "[core][types][ulong_t][benchmark][multiplication]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1200_ulong_t;
   ::stoneydsp::ulong_t b = 2_ulong_t;
 
-  BENCHMARK ("Multiplication") { return a * b; };
+  STONEYDSP_BENCHMARK ("Multiplication") { return a * b; };
 }
 
 // Benchmark for division
-TEST_CASE ("Benchmark for stoneydsp::ulong_t division",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][division]",
+                     "[core][types][ulong_t][benchmark][division]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1200_ulong_t;
   ::stoneydsp::ulong_t b = 2_ulong_t;
 
-  BENCHMARK ("Division") { return a / b; };
+  STONEYDSP_BENCHMARK ("Division") { return a / b; };
 }
 
 // Benchmark for type conversion to int
-TEST_CASE ("Benchmark for stoneydsp::ulong_t to int conversion",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][conversion][int]",
+                     "[core][types][ulong_t][benchmark][conversion][int]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1200_ulong_t;
 
-  BENCHMARK ("Conversion to int") { return static_cast<int> (a); };
+  STONEYDSP_BENCHMARK ("Conversion to int") { return static_cast<int> (a); };
 }
 
 // Benchmark for type conversion to float
-TEST_CASE ("Benchmark for stoneydsp::ulong_t to float conversion",
-           "[core][types][ulong_t][benchmark]")
+STONEYDSP_TEST_CASE ("[core][types][ulong_t][benchmark][conversion][float]",
+                     "[core][types][ulong_t][benchmark][conversion][float]")
 {
   using namespace ::stoneydsp::core::types::literals;
 
   ::stoneydsp::ulong_t a = 1200_ulong_t;
 
-  BENCHMARK ("Conversion to float")
+  STONEYDSP_BENCHMARK ("Conversion to float")
   {
-    return static_cast< ::stoneydsp::float_t> (a);
+    return static_cast<float> (a);
   };
 }
 
