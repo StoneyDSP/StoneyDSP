fd3247beb15721f8d12c7ccf4fdc715fe7a16320
StoneyDSP <nathanjhood@googlemail.com>
2025-03-06 13:28:06 +0100

    edit 'test/stoneydsp/core/types/long.test.cpp' - use relpath for utils header, use generic float for rounding test
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/stoneydsp/core/types/long.test.cpp
+++ test/stoneydsp/core/types/long.test.cpp
@@ -7,7 +7,7 @@
 
 //==============================================================================
 
-#include "stoneydsp/core/system/types.h"
+#include "stoneydsp/core/types/long.h"
 
 //==============================================================================
 
@@ -15,7 +15,7 @@
 
 //==============================================================================
 
-  #include "utils.test.hpp"
+  #include "../../../utils.test.hpp"
 
 //================================================================//requirements
 
@@ -189,7 +189,7 @@ STONEYDSP_TEST_CASE ("[core][types][long_t][endianness]",
 STONEYDSP_TEST_CASE ("[core][types][long_t][rounding]",
                      "[core][types][long_t][rounding]")
 {
-  ::stoneydsp::float_t a = 1.5f;
+  float a = 1.5f;
   ::stoneydsp::long_t b = static_cast< ::stoneydsp::long_t> (::std::round (a));
   STONEYDSP_REQUIRE (
       b == 2); // 1.5 rounded to nearest integer and cast to long_t
