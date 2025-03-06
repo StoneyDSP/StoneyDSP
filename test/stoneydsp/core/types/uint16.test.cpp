1871c618755b0a1b88bd615c625c8f50f0fcb50b
StoneyDSP <nathanjhood@googlemail.com>
2025-03-06 13:32:21 +0100

    edit 'test/stoneydsp/core/types/uint16.test.cpp' - use relpath for utils header, use generic float for rounding test
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/stoneydsp/core/types/uint16.test.cpp
+++ test/stoneydsp/core/types/uint16.test.cpp
@@ -7,7 +7,7 @@
 
 //==============================================================================
 
-#include "stoneydsp/core/system/types.h"
+#include <stoneydsp/core/system/types.h>
 
 //==============================================================================
 
@@ -15,7 +15,9 @@
 
 //==============================================================================
 
-  #include "utils.test.hpp"
+  #include "../../../utils.test.hpp"
+
+  #include <stoneydsp/core/types/ushrt.h> // for numerical_limits<ushrt_t>
 
 //======================================================================//sizeof
 
@@ -147,7 +149,7 @@ STONEYDSP_TEST_CASE ("[core][types][uint16_t][endianness]",
 STONEYDSP_TEST_CASE ("[core][types][uint16_t][rounding]",
                      "[core][types][uint16_t][rounding]")
 {
-  ::stoneydsp::float_t a = 1.5f;
+  float a = 1.5f;
   ::stoneydsp::uint16_t b
       = static_cast< ::stoneydsp::uint16_t> (::std::round (a));
   STONEYDSP_REQUIRE (
