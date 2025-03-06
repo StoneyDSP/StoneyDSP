f7f240e5eff9900af1a656d7b8a19d13578cd272
StoneyDSP <nathanjhood@googlemail.com>
2025-03-06 13:05:05 +0100

    add 'stoneydsp/core/types/double.h'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ include/stoneydsp/core/types/double.h
@@ -0,0 +1,168 @@
+/**
+ * @file double.h
+ * @author StoneyDSP (nathanjhood@googlemail.com)
+ * @brief
+ * @version @STONEYDSP_CORE_VERSION@
+ * @date 2025-01-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#pragma once
+
+#ifndef STONEYDSP_CORE_TYPES_DBL_H_INCLUDED
+  #define STONEYDSP_CORE_TYPES_DBL_H_INCLUDED
+
+//==============================================================================
+
+  #include "../../../stoneydsp/core/system/types.h"
+
+//==============================================================================
+
+/**
+ * @brief
+ *
+ */
+typedef STONEYDSP_DBL_T stoneydsp_double_t;
+
+//==============================================================================
+
+STONEYDSP_EXTERN_C STONEYDSP_INLINE STONEYDSP_CONSTEXPR STONEYDSP_DBL_T
+    STONEYDSP_PUBLIC_FUNCTION
+    stoneydsp_double_c (double value) STONEYDSP_NOEXCEPT
+{
+  return STONEYDSP_DBL_C (value);
+}
+
+STONEYDSP_EXTERN_C STONEYDSP_INLINE STONEYDSP_CONSTEXPR STONEYDSP_DBL_T
+    STONEYDSP_PUBLIC_FUNCTION
+    stoneydsp_double_max () STONEYDSP_NOEXCEPT
+{
+  return stoneydsp_double_c (STONEYDSP_DBL_MAX);
+}
+
+STONEYDSP_EXTERN_C STONEYDSP_INLINE STONEYDSP_CONSTEXPR STONEYDSP_DBL_T
+    STONEYDSP_PUBLIC_FUNCTION
+    stoneydsp_double_min () STONEYDSP_NOEXCEPT
+{
+  return stoneydsp_double_c (STONEYDSP_DBL_MIN);
+}
+
+STONEYDSP_EXTERN_C STONEYDSP_INLINE STONEYDSP_CONSTEXPR STONEYDSP_DBL_T
+    STONEYDSP_PUBLIC_FUNCTION
+    stoneydsp_double_lowest () STONEYDSP_NOEXCEPT
+{
+  return stoneydsp_double_c (-STONEYDSP_DBL_MAX);
+}
+
+//==============================================================================
+
+  #ifdef STONEYDSP_CXX
+
+//==============================================================================
+
+namespace stoneydsp
+{
+/** @addtogroup stoneydsp
+ *  @{
+ */
+
+namespace core
+{
+/** @addtogroup core
+ *  @{
+ */
+
+namespace types
+{
+/** @addtogroup types
+ * @{
+ */
+
+//==============================================================================
+
+/**
+ * @brief
+ *
+ */
+using double_t = ::stoneydsp_double_t;
+
+//==============================================================================
+
+namespace literals
+{
+  /** @addtogroup literals
+   * @{
+   */
+
+	 STONEYDSP_INLINE
+	 STONEYDSP_CONSTEXPR STONEYDSP_DBL_T STONEYDSP_PUBLIC_FUNCTION
+	 /**
+		* @brief
+		*
+		*/
+	 operator"" _double_t (long double value) STONEYDSP_NOEXCEPT
+	 {
+		 return ::stoneydsp_double_c (value);
+	 }
+
+	/// @} literals
+} // namespace literals
+
+/// @} types
+} // namespace types
+
+/// @} group core
+} //  namespace core
+
+//==============================================================================
+
+
+template <>
+STONEYDSP_PACKED_STRUCT_BEGIN struct STONEYDSP_API
+STONEYDSP_ALIGN (alignof (STONEYDSP_DBL_T)) numeric_limits<STONEYDSP_DBL_T>
+{
+
+  STONEYDSP_INLINE static STONEYDSP_CONSTEXPR STONEYDSP_DBL_T STONEYDSP_API
+  max () STONEYDSP_NOEXCEPT
+  {
+    return ::stoneydsp_double_max ();
+  }
+
+  STONEYDSP_INLINE static STONEYDSP_CONSTEXPR STONEYDSP_DBL_T STONEYDSP_API
+  min () STONEYDSP_NOEXCEPT
+  {
+    return ::stoneydsp_double_min ();
+  }
+
+  STONEYDSP_INLINE static STONEYDSP_CONSTEXPR STONEYDSP_DBL_T STONEYDSP_API
+  lowest () STONEYDSP_NOEXCEPT
+  {
+    return ::stoneydsp_double_lowest ();
+  }
+
+} STONEYDSP_PACKED_STRUCT_END;
+
+//==============================================================================
+
+/**
+ * @brief
+ *
+ */
+using double_t = ::stoneydsp::core::types::double_t;
+
+//==============================================================================
+
+/// @} group stoneydsp
+} //  namespace stoneydsp
+
+  //==============================================================================
+
+  #endif // STONEYDSP_CXX
+
+//==============================================================================
+
+  #endif // STONEYDSP_CORE_TYPES_DBL_H_INCLUDED
+
+//=========================================================================//EOF
